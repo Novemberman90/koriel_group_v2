4a60b3afdbca5abd89b14d6d1d745d7f9b698939
Novemberman90 <alexledovit@gmail.com>
2025-08-13 20:11:39 +0300

    add offer style and funtionals tab

--- app/js/main.js
+++ app/js/main.js
@@ -12,4 +12,33 @@ window.addEventListener('DOMContentLoaded', ()=>{
     document.body.classList.toggle('lock');
     MENUWRAP.classList.toggle('menu__wrap--active')
   }
+
+
+  // tabs
+
+  const tabBtn = document.querySelectorAll('.tab-nav__btn'),
+  tabItem = document.querySelectorAll('.tab__item'),
+  tabNav = document.querySelector('.tab-nav');
+  
+  tabBtn.forEach(button => {
+    button.addEventListener('click', (e)=>{
+      const tabId = button.getAttribute('data-btn');
+      
+       // Убираем активный класс у всех кнопок
+      tabBtn.forEach(btn => {
+        btn.classList.remove('tab-nav__btn--active');
+      });
+
+      // Добавляем активный класс нажатой кнопке
+      button.classList.add('tab-nav__btn--active');
+
+      // Скрываем все табы
+      tabItem.forEach(item => {
+        item.classList.remove('tab__item--active');
+      });
+
+      document.getElementById(tabId).classList.add('tab__item--active')
+    })
+  });
+
 })
